a1d96b0f06d51d117848f99db15046c0c0cb4443
Gurinder Singh <frederick.the.fool@gmail.com>
2018-06-18 11:04:32 +0530

    Multiple changes and cleanup.
    Using new println! and format! macros instead of writes! etc. Updated code to go with changes in different dependencies since the last commit.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #![feature(lang_items)]
 #![feature(link_args)]
 #![feature(compiler_builtins_lib)]
+#![feature(alloc)]
 
 #[allow(unused_attributes)] // The below attribute is needed to specify the entry point. Hence suppressing the warning
 #[link_args = "/ENTRY:efi_start"]
@@ -11,8 +12,9 @@ extern "C" {}
 
 extern crate rlibc;
 extern crate compiler_builtins;
-extern crate efi;
+#[macro_use] extern crate efi;
 extern crate http_efi;
+extern crate alloc;
 
 use efi::ffi;
 use efi::{
@@ -29,15 +31,12 @@ use efi::{
     SystemTable,
     net,
     init_env,
-    io
+    io::{self, Read, Write, BufRead, BufReader},
+    net::TcpStream,
 };
 
-use http_efi::Client;
-
-use core::fmt::Write;
-use efi::io::Read;
-
-use http_efi::Header;
+use http_efi::{Client, Header, Url};
+use alloc::{String, string::ToString};
 
 #[no_mangle]
 #[lang="panic_fmt"]
@@ -77,114 +76,105 @@ pub extern "win64" fn efi_start(image_handle: ffi::EFI_HANDLE,
                                 sys_table : *const ffi::EFI_SYSTEM_TABLE) -> isize {
 
     init_env(image_handle, sys_table);
-    let sys_table = SystemTable(sys_table);
-    let mut c = sys_table.console();
+    let mut sys_table = SystemTable::new(sys_table).expect("Failed to initialize system table");
 
-    write!(c, "Hello from UEFI\r\n");
+    if let Err(msg) = run(&mut sys_table) {
+        println!("Exiting: {}", msg);
+    };
+
+    0
+}
+
+fn run(sys_table: &mut SystemTable) -> Result<(), String> {
+    let mut c = efi::stdout();
+
+    println!("Hello from UEFI");
 
     let mut bs = sys_table.boot_services();
 
-    write!(c, "Testing DHCP/PXE\r\n");
+    println!("Testing DHCP/PXE");
     // TODO: see tianocore-edk2\NetworkPkg\UefiPxeBcDxe\PxeBcBoot.c file to know to implement PXE sequence especially the method PxeBcDiscoverBootFile
-    let pxe_protocol = bs.locate_protocol::<PxeBaseCodeProtocol>();
-    if let Err(e) = pxe_protocol {
-            write!(c, "failed: {}\r\n", e);
-            return 0;
-    }
-
-    let pxe_protocol = pxe_protocol.unwrap();
+    let pxe_protocol = bs.locate_protocol::<PxeBaseCodeProtocol>().map_err(|e| "failed to locate Pxe prtocol")?;
 
     let sp = core::mem::size_of::<PxeBaseCodeProtocol>();
-    write!(c, "PXE size: {}\r\n", sp);
+    println!("PXE size: {}", sp);
 
-    if !pxe_protocol.mode().started() {
-        write!(c, "Starting Pxe\r\n");
+    if !pxe_protocol.mode().unwrap().started() {
+        println!("Starting Pxe");
         pxe_protocol.start(false);
     }
     else {
-        write!(c, "Pxe already started\r\n");
+        println!("Pxe already started");
     }
 
-    write!(c, "Starting DHCP\r\n");
+    println!("Starting DHCP");
     match pxe_protocol.dhcp(false) {
-        Ok(r) => { write!(c, "Dhcp succeeded\r\n") },
-                // write!(c, "{:?}, {:?}, {:?}\r\n", pxe_protocol.mode().proxy_offer().as_dhcpv4().bootp_opcode(), pxe_protocol.mode().proxy_offer_received(), pxe_protocol.mode().pxe_discover_valid()) },
-        Err(e) => write!(c, "Dhcp failed: {:?}\r\n", e),
+        Ok(r) => { println!("Dhcp succeeded") },
+                // println!("{:?}, {:?}, {:?}", pxe_protocol.mode().proxy_offer().as_dhcpv4().bootp_opcode(), pxe_protocol.mode().proxy_offer_received(), pxe_protocol.mode().pxe_discover_valid()) },
+        Err(e) => return Err(format!("Dhcp failed - {}", e))
     };
 
     let info = DiscoverInfo::default();
     let dp = core::mem::size_of::<DiscoverInfo>();
-    write!(c, "DiscoverInfo size: {}\r\n", dp);
+    println!("DiscoverInfo size: {}", dp);
 
-    write!(c, "Starting Discover\r\n");
+    println!("Starting Discover");
     match pxe_protocol.discover(BootType::Bootstrap, BOOT_LAYER_INITIAL, false, Some(&info)) {
         Ok(_) =>  { 
-                    write!(c, "Discover succeeded\r\n");
-                    write!(c, "Boot file:\r\n");
-                    let boot_file = pxe_protocol.mode().proxy_offer().as_dhcpv4().bootp_boot_file();
+                    println!("Discover succeeded");
+                    print!("Boot file: ");
+                    let boot_file = pxe_protocol.mode().unwrap().proxy_offer().unwrap().as_dhcpv4().unwrap().bootp_boot_file();
                     for ch in boot_file.iter() {
-                        write!(c, "{}", *ch as char);
+                        print!("{}", *ch as char);
                     }
-                    write!(c, "\r\n")
+                    println!("")
                 },
 
-        Err(e) => write!(c, "Discover failed: {:?}\r\n", e),
+        Err(e) => return Err(format!("Discover failed - {}", e)),
     };
 
 
-    // write!(c, "Testing TCP4\r\n");
-    // let remote_ip = net::Ipv4Addr::new(10, 1, 10, 11);
-    // let remote_port = 8000;
+    println!("Testing HTTP");
 
-    // write!(c, "Connecting to {:?}:{}...\r\n", remote_ip, remote_port);
+    print!("Enter HTTP URL: ");
 
-    // let tp = core::mem::size_of::<DiscoverInfo>();
-    // write!(c, "Tcp4Stream size: {}\r\n", tp);
+    let stdin = efi::stdin();
+    let url = stdin.lines().next().unwrap().unwrap();
+    let url = Url::parse(url).map_err(|_| "bad url")?;
 
-    // net::Tcp4Stream::connect(net::SocketAddrV4::new(remote_ip, remote_port))
-    //     .and_then(|mut stream| {
-    //         write!(c, "Connected!\r\n").unwrap();
-    //         let buf = "GET / HTTP/1.1\r\n".as_bytes();
-    //         use io::Write;
-
-    //         stream.write(&buf);
-    //         stream.write("Content-Length: 0\r\n".as_bytes());
-    //         stream.write("\r\n".as_bytes());
-
-    //         write!(c, "Data sent\r\n");
-
-    //         write!(c, "Received response: ");
-    //         let mut rbuf = [0; 1024];
-    //         let mut rbuf = efi::Vec::new();
-    //         stream.read_to_end(&mut rbuf);
-
-    //         for ch in rbuf.iter() {
-    //             write!(c, "{}", *ch as char);
-    //         }
-
-    //         Ok(())
-    //     })
-    //     .or_else(|e| {
-    //         write!(c, "Got status code: {:?}\r\n", e);
-    //         Err(3)
-    //     });
-
-
-    write!(c, "Testing HTTP\r\n");
+    let mut authority = url.host().to_string();
+    if let Some(port) = url.port() {
+        authority += ":";
+        authority += &port.to_string();
+    }
     
-    let remote_ip = net::Ipv4Addr::new(10, 1, 10, 11);
-    let remote_port = 8000;
-
-    let sa = net::SocketAddrV4::new(remote_ip, remote_port);
-
-    match Client::connect(sa) {
+    match Client::connect(authority) {
         Ok(mut client) => {
-            write!(c, "HTTP client connected!\r\n").unwrap();
-            let headers = [ Header { name: "Range", value: "bytes=0-10".as_bytes() } ];
-            let resp = client.request("GET", "/Cargo.toml", &headers, None).unwrap();
+            println!("HTTP client connected!");
+            let mut offset = 0;
+            let mut req_size = 10;
+            for _i in 0..1 {
+                // let mut range_val = format!("bytes={}-{}", offset, offset + req_size);
+                // let headers = [ Header::new("Range", range_val) ];
+                match client.request("GET", url.path(), None, None) {
+                    Ok(mut resp) => {
+                        println!("Got status code {} {:?}", resp.status_code().to_u16(), resp.status_code());
+                        if resp.status_code().is_success() {
+                            let mut body = String::new();
+                            resp.read_to_string(&mut body);
+                            println!("Body:");
+                            println!("{}", body);
+                        }
+                    },
+                    Err(e) => { return Err(format!("Http client failed to connect - {}", e))},
+                };
+
+                offset += req_size;
+            }
+            println!("");
         },
-        Err(e) => write!(c, "Got status code: {:?}\r\n", e).unwrap()
+        Err(e) => println!("Got status code: {:?}", e)
     }
 
-   0
+    Ok(())
 }
