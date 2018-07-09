2cc77e435d39e47d0d457b2cb055a579af8db949
Gurinder Singh <frederick.the.fool@gmail.com>
2018-07-09 12:22:58 +0530

    Cleanup of a couple of things
    - Doing raw TCP without using an HTTP dependency.
    - Rearranged code so that it easier to read

--- src/main.rs
+++ src/main.rs
@@ -14,35 +14,98 @@ extern "C" {}
 extern crate rlibc;
 extern crate compiler_builtins;
 #[macro_use] extern crate efi;
-extern crate http_efi;
 #[macro_use] extern crate alloc;
 
 use efi::ffi;
 use efi::{
-    protocols::{
-        PxeBaseCodeProtocol, 
-        LoadFileProtocol, 
-        LoadedImageProtocol,
-        BootType, 
-        BOOT_LAYER_INITIAL, 
-        DiscoverInfo, 
-        SrvListEntry
-    },
-    boot_services::InterfaceType,
     SystemTable,
     net,
     init_env,
-    io::{self, Read, Write, BufRead, BufReader, Cursor, copy},
-    net::TcpStream,
-    EfiError,
+    io::{self, Read, BufRead},
     EfiErrorKind,
-    image,
 };
 
-use http_efi::{HttpClient, BufferedStream, Header, Url};
-use alloc::{String, string::ToString, Vec};
+use alloc::String;
 use core::str;
 
+
+// EFI entry point. This function is the one that the UEFI platform calls when this image is loaded.
+#[no_mangle]
+pub extern "win64" fn efi_start(image_handle: ffi::EFI_HANDLE,
+                                sys_table : *const ffi::EFI_SYSTEM_TABLE) -> isize {
+
+    init_env(image_handle, sys_table);
+    let mut sys_table = SystemTable::new(sys_table).expect("Failed to initialize system table");
+
+    if let Err(msg) = run(&mut sys_table) {
+        println!("Exiting: {}", msg);
+    };
+
+    0
+}
+
+fn run(_sys_table: &mut SystemTable) -> Result<(), String> {
+    println!("Hello from UEFI");
+    println!("");
+
+    if net::dhcp::cached_dhcp_config().unwrap_or(None).is_none() { // If there's cached config then DHCP has already happend. Otherwise we start it.
+        println!("Performing DHCP...");
+        let dhcp_config = net::dhcp::run_dhcp().map_err(|e| format!("Dhcp failed - {}", e))?;
+
+        println!("    Your IP: {}, Subnet mask: {}", dhcp_config.ip(), dhcp_config.subnet_mask());
+        if let Some(server_ip) =  dhcp_config.dhcp_server_ip() {
+            println!("    Server IP: {}", server_ip);
+        }
+    }
+
+    println!("");
+    println!("Testing TCP by sending HTTP request to the given addr");
+
+    print!("Enter addr to connect to (<host>:<port>): ");
+    let stdin = efi::stdin();
+    let addr = stdin.lines().next().unwrap().unwrap();
+
+    println!("Connecting to {}...", addr);
+
+    net::TcpStream::connect(addr)
+        .and_then(|mut stream| {
+            println!("Connected!");
+            let buf = "GET / HTTP/1.1".as_bytes();
+            use io::Write;
+
+            stream.write(&buf).unwrap();
+            stream.write("\r\n".as_bytes()).unwrap();
+            stream.write("Content-Length: 0\r\n".as_bytes()).unwrap();
+            stream.write("\r\n".as_bytes()).unwrap();
+
+            println!("Req sent");
+
+            println!("");
+            println!("Received resp: ");
+            let mut rbuf = [0_u8; 2048];
+
+            let read = stream.read(&mut rbuf).unwrap();
+
+            if read == 0 {
+                return Err(EfiErrorKind::NoResponse.into())
+            }
+
+            let resp = String::from_utf8_lossy(&rbuf[..read]).into_owned();
+
+            println!("{}", resp);
+
+            println!("");
+
+            Ok(())
+        })
+        .or_else(|e| {
+            Err(format!("Failed to connect. Status code: {:?}", e))
+        })?;
+
+    Ok(())
+}
+
+//---- The below code is required to make Rust compiler happy. Without it compilation will fail. ---- //
 #[no_mangle]
 #[lang="panic_fmt"]
 pub extern fn panic_fmt(_: ::core::fmt::Arguments, _: &'static str, _: u32) -> ! {
@@ -74,194 +137,4 @@ fn main() {
 #[lang = "start"]
 fn start(_main: *const u8, _argc: isize, _argv: *const *const u8) -> isize {
     0
-}
-
-#[no_mangle]
-pub extern "win64" fn efi_start(image_handle: ffi::EFI_HANDLE,
-                                sys_table : *const ffi::EFI_SYSTEM_TABLE) -> isize {
-
-    init_env(image_handle, sys_table);
-    let mut sys_table = SystemTable::new(sys_table).expect("Failed to initialize system table");
-
-    if let Err(msg) = run(&mut sys_table) {
-        println!("Exiting: {}", msg);
-    };
-
-    0
-}
-
-struct HttpReader {
-    http_client: HttpClient<BufferedStream>,
-    url: Url,
-    curr_pos: usize,
-}
-
-impl HttpReader {
-    // TODO: this should take Into<Url> for ergonomics
-    pub fn connect(url: Url) -> efi::Result<Self> {
-        let http_client = HttpClient::connect(url.authority())
-            .map_err::<EfiError, _>(|_| EfiErrorKind::DeviceError.into())?;
-
-        Ok(Self { http_client, url, curr_pos: 0 })
-    }
-}
-
-impl Read for HttpReader {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        if buf.len() == 0 {
-            return Ok(0)
-        }
-
-        let headers = [ Header::new("Range", format!("bytes={}-{}", self.curr_pos, self.curr_pos + buf.len() - 1)) ];
-        let mut response = match self.http_client.request("GET", self.url.path(), Some(&headers), None) {
-            Ok(res) => Ok(res),
-            Err(http_efi::Error::IoError(_)) | Err(http_efi::Error::ParseFailure) => {
-                self.http_client = HttpClient::connect(self.url.authority())
-                    .map_err::<io::Error, _>(|_| io::ErrorKind::NotConnected.into())?;
-                Ok(self.http_client.request("GET", self.url.path(), Some(&headers), None) 
-                    .map_err::<io::Error, _>(|_| io::ErrorKind::Other.into())?)
-            },
-            Err(e) => Err(e),
-        }.expect("Response must be valid here");
-
-        if !response.status_code().is_success() {
-            return Err(io::ErrorKind::Other.into())
-        }
-
-        let mut total_bytes_read = 0;
-        loop {
-            let bytes_read = response.read(&mut buf[total_bytes_read..])?;
-            total_bytes_read += bytes_read;
-
-            if bytes_read == 0 {
-                break;
-            }
-        }
-
-        self.curr_pos += total_bytes_read;
-
-        Ok(total_bytes_read)
-    }
-}
-
-impl image::Len for HttpReader {
-    fn len(&mut self) -> efi::Result<usize> {
-        let mut response = self.http_client.request("HEAD", self.url.path(), None, None)
-            .map_err::<EfiError, _>(|_| EfiErrorKind::DeviceError.into())?;
-        if !response.status_code().is_success() {
-            return Err(EfiErrorKind::DeviceError.into())
-        }
-
-        let content_len_hdr_buf = &response.headers().iter().find(|h| h.name.eq_ignore_ascii_case("content-length"))
-            .ok_or::<EfiError>(EfiErrorKind::DeviceError.into())?
-            .value;
-        let content_len_val = str::from_utf8(&content_len_hdr_buf)
-            .map_err::<EfiError, _>(|_| EfiErrorKind::DeviceError.into())?;
-        let len = content_len_val.parse()
-            .map_err::<EfiError, _>(|_| EfiErrorKind::DeviceError.into())?;
-
-        Ok(len)
-    }
-}
-
-fn run(sys_table: &mut SystemTable) -> Result<(), String> {
-    let mut c = efi::stdout();
-
-    println!("Hello from UEFI");
-
-    let mut bs = sys_table.boot_services();
-
-    println!("Testing DHCP/PXE");
-    // TODO: see tianocore-edk2\NetworkPkg\UefiPxeBcDxe\PxeBcBoot.c file to know to implement PXE sequence especially the method PxeBcDiscoverBootFile
-    let pxe_protocol = bs.locate_protocol::<PxeBaseCodeProtocol>().map_err(|e| "failed to locate Pxe prtocol")?;
-
-    let sp = core::mem::size_of::<PxeBaseCodeProtocol>();
-    println!("PXE size: {}", sp);
-
-    if !pxe_protocol.mode().unwrap().started() {
-        println!("Starting Pxe");
-        pxe_protocol.start(false);
-    }
-    else {
-        println!("Pxe already started");
-    }
-
-    println!("Starting DHCP");
-    match pxe_protocol.dhcp(false) {
-        Ok(r) => { println!("Dhcp succeeded") },
-                // println!("{:?}, {:?}, {:?}", pxe_protocol.mode().proxy_offer().as_dhcpv4().bootp_opcode(), pxe_protocol.mode().proxy_offer_received(), pxe_protocol.mode().pxe_discover_valid()) },
-        Err(e) => return Err(format!("Dhcp failed - {}", e))
-    };
-
-    // let info = DiscoverInfo::default();
-    // let dp = core::mem::size_of::<DiscoverInfo>();
-    // println!("DiscoverInfo size: {}", dp);
-
-    // println!("Starting Discover");
-    // match pxe_protocol.discover(BootType::Bootstrap, BOOT_LAYER_INITIAL, false, Some(&info)) {
-    //     Ok(_) =>  { 
-    //                 println!("Discover succeeded");
-    //                 print!("Boot file: ");
-    //                 let boot_file = pxe_protocol.mode().unwrap().proxy_offer().unwrap().as_dhcpv4().unwrap().bootp_boot_file();
-    //                 for ch in boot_file.iter() {
-    //                     print!("{}", *ch as char);
-    //                 }
-    //                 println!("")
-    //             },
-
-    //     Err(e) => return Err(format!("Discover failed - {}", e)),
-    // };
-
-    println!("Testing HTTP");
-
-   print!("Enter URL to boot from (or press ENTER for default): ");
-
-    let stdin = efi::stdin();
-    let mut url = stdin.lines().next().unwrap().unwrap();
-    if url.is_empty() {
-        url = "http://client2.ntl.local:8001/wimboot".to_string();
-    }
-
-    let url = Url::parse(url).map_err(|_| "bad url")?;
-    let mut authority = url.host().to_string();
-    if let Some(port) = url.port() {
-        authority += ":";
-        authority += &port.to_string();
-    }
-
-    println!("Booting from '{}'", url);
-    let mut reader = HttpReader::connect(url).map_err(|_| "failed to connect HTTP reader")?;
-    println!("Connected");
-    let loaded_image = image::load_image(&mut reader).map_err(|e| format!("failed to load image: {}", e))?;
-    println!("Image loaded");
-    let exit_data = image::start_image(&loaded_image).map_err(|_| "failed to start image")?;
-    
-    // println!("Doing GET on '{}'", url);
-    // match HttpClient::connect(authority) {
-    //     Ok(mut client) => {
-    //         println!("HTTP client connected!");
-    //         let mut offset = 0;
-    //         let mut req_size = 10;
-    //         for _i in 0..1 {
-    //             match client.request("GET", url.path(), None, None) {
-    //                 Ok(mut resp) => {
-    //                     println!("Got status code {} {:?}", resp.status_code().to_u16(), resp.status_code());
-    //                     if resp.status_code().is_success() {
-    //                         let mut body = String::new();
-    //                         resp.read_to_string(&mut body);
-    //                         println!("Body:");
-    //                         println!("{}", body);
-    //                     }
-    //                 },
-    //                 Err(e) => { return Err(format!("Http client failed to send request - {}", e))},
-    //             };
-
-    //             offset += req_size;
-    //         }
-    //         println!("");
-    //     },
-    //     Err(e) => return Err(format!("Failed to connect - {}", e))
-    // }
-
-    Ok(())
 }
